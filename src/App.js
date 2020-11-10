aeb7e8d90c86bc074ab6381037f380a4c9d7053c
CoderS2020 <71634100+CoderS2020@users.noreply.github.com>
2020-11-10 20:40:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Review from './Review';
+
+function App() {
+  return <main className='container' >
+    <section className='title'>
+      <h2>our reviews</h2>
+      <div className='underline'></div>
+      <Review/>
+    </section>
+  </main>
+}
+
+export default App;
